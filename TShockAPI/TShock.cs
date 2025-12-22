73cd50af13f90e3b3e351ea786de9c2d702a2a30
Cai <13110818005@qq.com>
2025-12-23 00:31:38 +0800

    refactor: replace magic numbers with PacketTypes enum chore: remove dead check in OnGetData

--- TShockAPI/TShock.cs
+++ TShockAPI/TShock.cs
@@ -1645,8 +1645,10 @@ namespace TShockAPI
 				return;
 			}
 
-			if ((player.State < (int)ConnectionState.Complete || player.Dead) && (int)type > 12 && (int)type != 16 && (int)type != 42 && (int)type != 50 &&
-				(int)type != 38 && (int)type != 21 && (int)type != 22 && type != PacketTypes.SyncLoadout)
+			if ((player.State < (int)ConnectionState.Complete) &&
+			    type > PacketTypes.PlayerSpawn && type != PacketTypes.PlayerHp && type != PacketTypes.PlayerMana &&
+			    type != PacketTypes.PlayerBuff && type != PacketTypes.PasswordSend && type != PacketTypes.ItemDrop &&
+			    type != PacketTypes.ItemOwner && type != PacketTypes.SyncLoadout)
 			{
 				e.Handled = true;
 				return;
